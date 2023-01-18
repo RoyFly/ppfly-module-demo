c581d2c244367af43d8c37317e5f3f4cfb6511eb
Your Name <you@example.com>
2023-01-18 17:50:16 +0800

    feat: 动态修改logback日志级别

--- logger/src/main/java/com/logger/controller/LoggerController.java
+++ logger/src/main/java/com/logger/controller/LoggerController.java
@@ -20,22 +20,39 @@ public class LoggerController {
     private LoggerService loggerService;
 
     /**
+     * 入参可以为：ERROR、WARN、INFO、DEBUG、TRACE
+     *
      * @param logLevel
      * @return
      */
-    @GetMapping(value = "/level/{logLevel}")
-    public String changeLogLevel(@PathVariable("logLevel") String logLevel) {
+    @GetMapping(value = "/level/{fullClassName}/{logLevel}")
+    public String changeLogLevel(@PathVariable("fullClassName") String fullClassName, @PathVariable("logLevel") String logLevel) {
         try {
             // 1. 获取日志上下文
             LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
             // 2. 修改指定logger对象的日志级别(这里根据你自己的日志名称修改，我的叫"my_logger")
             // 如果修改mybatis、spring框架的日志，写"org.mybatis"、"org.springframework"
-            loggerContext.getLogger("my_logger").setLevel(Level.valueOf(logLevel));
+            loggerContext.getLogger(fullClassName).setLevel(Level.valueOf(logLevel));
             log.info("日志级别修改成功!" + logLevel);
         } catch (Exception e) {
             log.error("动态修改日志级别出错", e);
             return "failure";
         }
+        return "success";
+    }
+
+    /**
+     * 测试打印日志
+     *
+     * @return
+     */
+    @GetMapping(value = "/testLog")
+    public String testLog() {
+        log.trace("trace级别的日志，tracetracetrace.........");
+        log.debug("debug级别的日志，debugdebugdebug.........");
+        log.info("info级别的日志，infoinfoinfo.........");
+        log.warn("warn级别的日志，warnwarnwarn.........");
+        log.error("error级别的日志，errorerrorerror.........");
         return "success";
     }
 
